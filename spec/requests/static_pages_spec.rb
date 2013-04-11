8087561ea5804f9ea17680cabe3ccd3ddc263d20
Michele Cenzer <michele_cenzer@mac.com>
2013-04-11 17:27:08 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,6 @@ describe "StaticPages" do
 
   shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
   end
 
   describe "Home page" do
