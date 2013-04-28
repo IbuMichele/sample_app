1645ad154093535609d4860158822a99cc76ad60
Michele Cenzer <michele_cenzer@mac.com>
2013-04-28 14:23:38 -0400

    Finish sign in

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-
 	describe "full title" do
 		it "should include the page title" do
 			full_title("something").should =~ /something/
 		end
 
 		it "should include the base title" do
-			full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+			full_title("fred").should =~ /^Ruby on Rails Tutorial Sample App/
 		end
 
 		it "should not include a bar for the home page" do
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,19 +32,14 @@ describe "UserPages" do
 
 		  describe "after submission" do
 		  	
-		  	before {click_button submit }
+		  	before { click_button submit }
 
 			it { should have_selector('title', text: 'Sign up') }
 			it { should have_content('error') }
 		end
 
 		describe "with valid information" do
-			before do
-				fill_in "Name",			with: "Example User"
-				fill_in "Email",		with: "user@example.com"
-				fill_in "Password",		with: "foobar"
-				fill_in "Confirmation", with: "foobar"
-			end
+			before { valid_new_user }
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
@@ -55,7 +50,7 @@ describe "UserPages" do
 				let(:user) { User.find_by_email('user@example.com') }
 
 				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				it { should have_success_message('Welcome') }
 				it { should have_link('Sign out') }
 			end
 		end
